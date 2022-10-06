4adaa3bcf54cf5838c976b129c97aa6d94fd20b1
bewzunjo <saran_kim@cmu.ac.th>
2022-10-06 15:35:08 +0700

    odey

--- pages/api/withdraw.js
+++ pages/api/withdraw.js
@@ -1,7 +1,13 @@
+import { checkToken } from "../../backendLibs/checkToken";
+import { readUsersDB, writeUsersDB } from "../../backendLibs/dbLib";
+
 export default function withdrawRoute(req, res) {
   if (req.method === "PUT") {
-    //check authentication
-    //return res.status(403).json({ ok: false, message: "You do not have permission to withdraw" });
+    const user = checkToken(req);
+    if (!user || user.isAdmin)
+      return res
+        .status(403)
+        .json({ ok: false, message: "You do not have permission to withdraw" });
 
     const amount = req.body.amount;
     //validate body
@@ -9,12 +15,26 @@ export default function withdrawRoute(req, res) {
       return res.status(400).json({ ok: false, message: "Invalid amount" });
 
     //check if amount < 1
-    // return res.status(400).json({ ok: false, message: "Amount must be greater than 0" });
+    if (amount < 0)
+      return res
+        .status(400)
+        .json({ ok: false, message: "Amount must be greater than 0" });
 
     //find and update money in DB (if user has enough money)
-    //return res.status(400).json({ ok: false, message: "You do not has enough money" });
+
+    const users = readUsersDB();
+    const userIdx = users.findIndex((x) => x.username === user.username);
+    if (users[userIdx].money < amount)
+      return res
+        .status(400)
+        .json({ ok: false, message: "You do not has enough money" });
+    //please ignore the grammatical error
+
+    users[userIdx].money -= amount;
+    writeUsersDB(users);
 
     //return response
+    return res.json({ ok: true, money: users[userIdx].money });
   } else {
     return res.status(400).json({ ok: false, message: "Invalid HTTP Method" });
   }
